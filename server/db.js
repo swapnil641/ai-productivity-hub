02ea445137e0e42afcfe1868dec74397d22c441e
swapnil641 <swapnilpawar641@gmail.com>
2026-06-16 13:49:43 +0530

    Fix PostgreSQL Pool import

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "postgres",
+  host: process.env.DB_HOST || "postgres",
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || "todo_db",
 });
 
 module.exports = pool;

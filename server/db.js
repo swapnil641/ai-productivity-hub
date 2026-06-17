e7a8e16fabbfbd2d3021e124bec4f8ea690a0760
swapnil641 <swapnilpawar641@gmail.com>
2026-06-17 09:54:55 +0530

    Expose backend port 5000

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "postgres",
-  host: process.env.DB_HOST || "postgres",
+  host: process.env.DB_HOST || "ubuntu@ec2-13.233.146.16",
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || "todo_db",
 });
